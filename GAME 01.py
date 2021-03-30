ee1c7fc894ec3a3d5bc29c2c48b6677505b9c063
VICTOR J <victorjerrysson@gmail.com>
2021-03-30 20:05:18 -0300

    Retangulo mundando de posição ao contato

--- GAME 01.py
+++ GAME 01.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 pygame.init()
 
@@ -8,20 +9,23 @@ largura = 640
 altura = 480
 x = largura / 2
 y = altura / 2
+
+x_azul = randint(40, 600)
+y_azul = randint(50, 430)
+
 relogio = pygame.time.Clock()
-
 tela = pygame.display.set_mode((largura, altura))
-
 pygame.display.set_caption('Jogo')
 
 #Código principal do Game
 while True:
-    relogio.tick(60)#Controle de FPS
+    relogio.tick(30)#Controle de FPS
     tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+        '''
         if event.type == KEYDOWN:
             if event.key == K_a:
                 x -= 20
@@ -31,8 +35,24 @@ while True:
                 y += 20
             elif event.key == K_w:
                 y -= 20
-    #Desenhando os objetos
-    pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
+        '''
+    if pygame.key.get_pressed()[K_a]:
+        x -= 20
+    
+    elif pygame.key.get_pressed()[K_d]:
+        x += 20
+    
+    elif pygame.key.get_pressed()[K_s]:
+        y += 20
+    
+    elif pygame.key.get_pressed()[K_w]:
+        y -= 20
 
+    #Desenhando os objetos
+    ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
+    ret_azul = pygame.draw.rect(tela ,(0,0,255), (200,300,40,50))
+
+    if ret_vermelho.colliderect(ret_azul):
+        print('COLISÃO')
 
     pygame.display.update()
